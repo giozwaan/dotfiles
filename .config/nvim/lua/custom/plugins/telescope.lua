0f3fbd2978da96b7228d4d4d5fd5d32d51e97d0e
giozwaan <giozwaan@hotmail.nl>
2025-11-13 21:28:50 +0100

    nvim lsp keymaps

--- .config/nvim/lua/custom/plugins/telescope.lua
+++ .config/nvim/lua/custom/plugins/telescope.lua
@@ -16,5 +16,15 @@ return {
 
         vim.keymap.set("n", "<space>ff", builtin.find_files)
         vim.keymap.set("n", "<space>gw", builtin.grep_string)
+
+        vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
+        vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+        vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+
+        vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
+        vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
+        vim.keymap.set("n", "<space>wd", builtin.lsp_document_symbols, { buffer = 0 })
     end
 }
