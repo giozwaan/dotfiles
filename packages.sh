2d80d17b613dd3b746a3c14cc26bc02336a371eb
giozwaan <giozwaan@hotmail.nl>
2025-11-15 23:00:04 +0100

    install script fixes

--- packages.sh
+++ packages.sh
@@ -1,21 +1,18 @@
 pacman -S --needed \
 	pipewire wireplumber pipewire-alsa pipewire-jack pipewire-pulse \
-	bluez bluez-utils network-manager wpa_supplicant \
+	bluez bluez-utils networkmanager wpa_supplicant \
 	firefox openssh libreoffice alacritty git vim \
-	libvirt power-profiles-daemon sudo efibootmgr grub
+	libvirt qemu power-profiles-daemon sudo efibootmgr grub \
 
-
-pacman -S --needed \
 	sway swaylock swayidle swaybg waybar wofi \
 	qt5ct qt6ct qt5-wayland qt6-wayland xorg-xwayland breeze-gtk breeze-icons \
 	xdg-desktop-portal xdg-desktop-portal-wlr xdg-utils xdg-user-dirs \
 	pavucontrol dolphin otf-font-awesome noto-fonts-cjk
 
-# pacman -S --needed gnome gdm gnome-shell-extensions gnome-tweaks
+	# gnome gdm gnome-shell-extensions gnome-tweaks
 
-# pacman -S --needed \
-# 	plasma-desktop sddm dolphin \
-# 	kinfocenter kscreen plasma-nm plasma-pa
+	# plasma-desktop sddm dolphin \
+	# kinfocenter kscreen plasma-nm plasma-pa kwalletmanager
 
 
 systemctl enable NetworkManager
